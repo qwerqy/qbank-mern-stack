37a705c1408132de286b1cfcc74831d6a12a945b
Amin Roslan <amin@VaseAdmins-MBP.lan>
2018-11-13 16:59:47 +0800

    added navbar/container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Navbar from "./components/Navbar";
+import SearchContainer from "./components/SearchContainer";
+
 class App extends Component {
   render() {
-    return <div className="App">Testing</div>;
+    return (
+      <div>
+        <Navbar />
+        <SearchContainer />
+      </div>
+    );
   }
 }
 
